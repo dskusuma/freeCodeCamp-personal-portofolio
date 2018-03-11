48213fb03fe349ed085929bd8b827bd56f5c5c9b
dskusuma <dwisatriakusuma4@gmail.com>
2018-03-11 21:13:02 +0700

    update home and js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    //jQuery to collapse navbar
+    $(window).scroll(function(){
+        if($(".navbar").offset().top > 50){
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    })
+});
